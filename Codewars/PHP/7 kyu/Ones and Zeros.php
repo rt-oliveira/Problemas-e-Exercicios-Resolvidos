7c2d9096a057b69ba4a5f31c2b27d84a7fdb29ea
Rafael Teixeira <fael.teixeira@gmail.com>
2021-04-12 22:31:51 -0300

    Add 'Ones and Zeros' (Codewars, PHP).

---
+++ Codewars/PHP/7 kyu/Ones and Zeros.php
@@ -0,0 +1,29 @@
+<?php
+	/* Descrição:
+
+	Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+Examples:
+
+Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 0, 1] ==> 5
+Testing: [1, 0, 0, 1] ==> 9
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 1, 0] ==> 6
+Testing: [1, 1, 1, 1] ==> 15
+Testing: [1, 0, 1, 1] ==> 11
+However, the arrays can have varying lengths, not just limited to 4.
+*/
+
+function binaryArrayToNumber($arr) {
+  $result = 0;
+  for ($i = 0; $i < sizeof($arr); $i++){
+    $potencia = (sizeof($arr) - ($i+1));
+    $result += $arr[$i] * (2 ** $potencia);
+  }
+  return $result;
+}
+?>
